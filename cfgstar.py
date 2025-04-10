37c0b2d08aad048e65876874c8c6c95a94e9d6a0
bvhari <bvharibv@hotmail.com>
2025-04-10 23:22:58 +0530

    Implement as a post cfg function

--- cfgstar.py
+++ cfgstar.py
@@ -20,17 +20,16 @@ class CFGStar:
             cond = args["cond_denoised"]
             uncond = args["uncond_denoised"]
             cond_scale = args["cond_scale"]
-            x = args["input"]
 
             dot_product = torch.sum(cond * uncond, dim=(-1, -2))
             squared_norm = torch.sum(uncond * uncond, dim=(-1, -2)) + 1e-8
             s = dot_product / squared_norm
             uncond_scaled = uncond * s[:, :, None, None]
 
-            return x - (uncond_scaled + cond_scale * (cond - uncond_scaled))
+            return uncond_scaled + cond_scale * (cond - uncond_scaled)
 
         m = model.clone()
-        m.set_model_sampler_cfg_function(custom_cfg_function)
+        m.set_model_sampler_post_cfg_function(custom_cfg_function)
 
         return (m,)
 
